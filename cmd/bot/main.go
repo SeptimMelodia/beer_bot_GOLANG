66fd724afa5989f7d406b15b6ea2baad6e0df7b5
viktor <stune@student.21-school.ru>
2023-07-23 08:33:22 +0300

    viper

--- cmd/bot/main.go
+++ cmd/bot/main.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	"log"
+	"tg_bot_beer_to_peer/pkg/repository"
 	"tg_bot_beer_to_peer/pkg/telegram"
 
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+	"github.com/spf13/viper"
 )
 
 func main() {
@@ -12,6 +14,26 @@ func main() {
 	if err != nil {
 		log.Panic(err)
 	}
+
+	viper.SetConfigName("config")
+	viper.SetConfigType("yaml")
+	viper.AddConfigPath("./configs")
+	err = viper.ReadInConfig()
+	if err != nil {
+		log.Println(err)
+	}
+	log.Println(viper.GetString("db.host"))
+	db, err := repository.NewMySqlDB(repository.Config{
+		Host:     viper.GetString("db.host"),
+		Port:     viper.GetString("db.port"),
+		Username: viper.GetString("db.username"),
+		Password: viper.GetString("db.password"),
+		DBName:   viper.GetString("db.dbname"),
+	})
+	if err != nil {
+		log.Fatalf("failed to initiaslize db: %s", err.Error())
+	}
+	repository.NewRepository(db)
 
 	bot.Debug = true
 
--- pkg/repository/mysql.go
+++ pkg/repository/mysql.go
@@ -1,3 +1,29 @@
 package repository
 
-//func NewMySqlDB() (*sql)
+import (
+	"fmt"
+
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/jmoiron/sqlx"
+)
+
+type Config struct {
+	Host     string
+	Port     string
+	Username string
+	Password string
+	DBName   string
+}
+
+func NewMySqlDB(conf Config) (*sqlx.DB, error) {
+	db, err := sqlx.Open("mysql", fmt.Sprintf("%s:%s@tcp(%s:%s)/%s", conf.Username, conf.Password, conf.Host, conf.Port, conf.DBName))
+	if err != nil {
+		return nil, err
+	}
+
+	err = db.Ping()
+	if err != nil {
+		return nil, err
+	}
+	return db, nil
+}
